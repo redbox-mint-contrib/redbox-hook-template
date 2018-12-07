8fb2cd0b7093e2639df08c4295350ba2b420b998
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2018-12-07 12:46:07 +1030

    Added travis configuration and templates for redbox-dev-tools

--- support/development/linkHook.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-cd /opt/sailshook; 
-yarn link; 
-cd /opt/redbox-portal;  
-if [ ! -d "node_modules/@uts-eresearch/sails-hook-redbox-template" ]; then
-    yarn add  "file:/opt/sailshook";
-fi
-yarn link "@uts-eresearch/sails-hook-redbox-template"; 
-node app.js
