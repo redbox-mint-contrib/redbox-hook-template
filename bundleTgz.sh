11171d5c0e1d828199e1be2243fda4eb33675216
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2018-12-12 20:30:35 +1030

    Added line to clean up npm packaged tgz after we're finished with it

--- bundleTgz.sh
+++ bundleTgz.sh
@@ -5,10 +5,12 @@ mkdir tmp
 mkdir dist
 echo "Extracting npm made tarball to tmp dir so we can remove the internal package dir"
 tar xvfz $fileName -C tmp/
+rm -f $fileName
 cd tmp/package
 echo "Repackaging tarball without package directory"
 tar cvfz ../../dist/$fileName .
 cd ../../
 rm -Rf tmp
+
 
 
